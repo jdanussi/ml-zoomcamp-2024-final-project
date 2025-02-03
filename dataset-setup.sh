689f4a5320ab3f5368418a980e4428d6b751fd61
jdanussi <jdanussi@gmail.com>
2025-02-02 23:19:29 -0300

    updated dataset-setup.sh

--- dataset-setup.sh
+++ dataset-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p dataset && \
-#wget https://www.robots.ox.ac.uk/%7Evgg/data/flowers/102/102flowers.tgz && \
+wget https://www.robots.ox.ac.uk/%7Evgg/data/flowers/102/102flowers.tgz && \
 tar -xvzf 102flowers.tgz -C dataset
 wget -P dataset https://www.robots.ox.ac.uk/%7Evgg/data/flowers/102/imagelabels.mat
 
